06db5cc5259d9ea16c5a3527f2bf61f9a92fa8f4
Dogue <dogue@oddhelix.com>
2023-12-05 23:25:37 -0500

    batman

---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+use types::Type;
+
+mod types;
+
+#[derive(Debug)]
+pub struct FifVM {
+    pub stack: Vec<Type>,
+}
+
+impl FifVM {
+    pub fn new() -> Self {
+        Self { stack: Vec::new() }
+    }
+
+    pub fn run(&mut self) {}
+
+    fn pop(&mut self) -> Type {
+        self.stack.pop().unwrap()
+    }
+
+    fn pop_two(&mut self) -> (Type, Type) {
+        (self.pop(), self.pop())
+    }
+
+    fn push(&mut self, t: Type) {
+        self.stack.push(t);
+    }
+
+    fn add(&mut self) {
+        let (a, b) = self.pop_two();
+        self.push(a + b);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_add_int() {
+        let mut vm = FifVM::new();
+        vm.push(Type::Int(1));
+        vm.push(Type::Int(2));
+        vm.add();
+
+        assert_eq!(vm.stack[0], Type::Int(3));
+    }
+
+    #[test]
+    fn test_add_float() {
+        let mut vm = FifVM::new();
+        vm.push(Type::Float(1.0));
+        vm.push(Type::Float(2.0));
+        vm.add();
+
+        assert_eq!(vm.stack[0], Type::Float(3.0));
+    }
+}
---
+++ src/types.rs
@@ -0,0 +1,42 @@
+use std::{ops::Add, str::FromStr};
+
+#[derive(Debug, PartialEq)]
+pub enum Type {
+    Int(isize),
+    Float(f32),
+    Str(String),
+    Invalid,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ParseTypeError(String);
+
+impl FromStr for Type {
+    type Err = ParseTypeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.contains('.') {
+            let float = s
+                .parse::<f32>()
+                .map_err(|e| ParseTypeError(e.to_string()))?;
+            Ok(Type::Float(float))
+        } else {
+            let int = s
+                .parse::<isize>()
+                .map_err(|e| ParseTypeError(e.to_string()))?;
+            Ok(Type::Int(int))
+        }
+    }
+}
+
+impl Add for Type {
+    type Output = Type;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (Type::Int(a), Type::Int(b)) => Type::Int(a + b),
+            (Type::Float(a), Type::Float(b)) => Type::Float(a + b),
+            _ => Type::Invalid,
+        }
+    }
+}
