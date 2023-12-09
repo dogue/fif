f1fdac495e372e07418ff4e7d15c168c4f22a090
Dogue <dogue@oddhelix.com>
2023-12-08 22:10:41 -0500

    update readme and example in main.rs

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ A stack-based programming language inspired by FORTH, but intended to be more ap
 This repo is extremely early in development and is nowhere near usable yet. Feel free to poke around and even try it out if you want, but be aware that there are no docs. You'll just have to peruse the code and figure it out for now.
 
 Currently, basic arithmetic operations work for both integers and floats, as well as string concatenation (using the addition operator `+`). Swapping and duplicating stack values (using the `swap` and `dupe` keywords, respectively) also works.
+
+`main.rs` has a very simple example program in a raw string that you can tinker with. Expect syntax errors to panic.
--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,12 @@ fn main() {
     let mut vm = FifVM::new();
     vm.run(
         r#"
-        1 2 swap dupe
+        1 2      // numeric literals get pushed on to the stack
+        +        // simple addition, now 3 is at the top of the stack
+        dupe     // duplicate the 3 on the top of the stack
+        *        // multiply the 3's together, now 9 is on top of the stack
+        5.0 /    // if you mix floats and ints, the result will be a float
         "#,
     );
     println!("{vm:?}");
-    // FifVM { stack: [Str("hello world"), Float(420.69)] }
-
-    // let mut lexer = Lexer::new(
-    //     r#"
-    //         1 2 +    // add one and two
-    //         1.3
-    //         dupe     // duplicate the 3
-    //         *        // multiply the two 3's
-    //     "#,
-    // );
-
-    // loop {
-    //     let token = lexer.next_token().unwrap();
-    //     println!("{token:?}");
-    //     if token == Token::Eof {
-    //         break;
-    //     }
-    // }
 }
